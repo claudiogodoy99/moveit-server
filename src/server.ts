289c6e1f051ee8236dae8347e9b0f251366a3b31
Claudio Godoy <40471021+claudiogodoy99@users.noreply.github.com>
2021-03-08 14:26:57 -0300

    configuracao do mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import express from 'express'
+import user from '@models/User'
 
 const app = express()
 
-app.get('/', (req, resp) => {
-  return resp.status(200).json({
-    message: 'helo world'
-  })
+app.get('/', async (req, resp) => {
+  try {
+    await user.create({
+      name: 'claudio',
+      email: 'godoy@gmail.com'
+    })
+
+    return resp.status(200).json({
+      message: 'helo world'
+    })
+  } catch (ex) {
+    return resp.status(400).json({
+      message: ex.message
+    })
+  }
 })
 
 app.listen(8080)
